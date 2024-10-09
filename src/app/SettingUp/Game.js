c9553758e178fe18e1a8bdd099d40392ad7b0157
Iniyan Ilangovan <106104633+IniyanIlan@users.noreply.github.com>
2024-10-09 02:37:41 -0500

    feature: chaging UI for figma, buttons added, leaderboard needs work

--- src/app/SettingUp/Game.js
+++ src/app/SettingUp/Game.js
@@ -2,19 +2,25 @@ import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import Leaderboard from '../Tracking/Leaderboard';
 import Excavation from '../Action/Excavation';
+import './Game.css';
 
 const Game = () => {
     const { state } = useLocation();
     return(
-        <>
-        <div>
-            <h1 style={
-                {textAlign: 'center'}
-            }>Game Page</h1>
-            <Leaderboard playerNames={state.playerNames}/>
+        <div className = "game">
+            <div className = "box">
+                <a href="/Rules" target="_blank">Rule Book</a>
+                <a href="/StartGame">Game Log</a>
+                <a href = "/NumberPlayer">Restart Game</a>
+                <a href = "/" class="push">Quit</a>
+            </div>
+            
             <Excavation></Excavation>
+            <div className = "leaderboard">
+                <Leaderboard playerNames={state.playerNames}/>
+            </div>
+
         </div>
-        </>
     );
 }
 
