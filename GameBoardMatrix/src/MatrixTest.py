c0c7caf295ec153fa3f4e6dc5c51acdb0f3f0cfd
RPI5 <tidesoftreachery2025@gmail.com>
2024-10-14 06:25:15 +0100

    final Matrix test

--- GameBoardMatrix/src/MatrixTest.py
+++ GameBoardMatrix/src/MatrixTest.py
@@ -7,6 +7,7 @@ def main():
     #Initialize 2d array
     rows, cols = (2,2)
     gameBoard = [[0] * cols] * rows
+    n = 0
     while(not GPIO.input(10)):
         n += 1
         n -= 1 # do nothing
@@ -23,23 +24,23 @@ def matrixInit():
     GPIO.setup(7, GPIO.IN, GPIO.PUD_DOWN) # pos 3
     GPIO.setup(8, GPIO.IN, GPIO.PUD_DOWN) # pos 4
     # Set edge detection functions
-    GPIO.add_event_detect(3, GPIO.RISING, setTopLeft, 20)
-    GPIO.add_event_detect(5, GPIO.RISING, setTopRight, 20)
-    GPIO.add_event_detect(7, GPIO.RISING, setBotLeft, 20)
-    GPIO.add_event_detect(8, GPIO.RISING, setBotRight, 20)
+    GPIO.add_event_detect(3, GPIO.FALLING, setTopLeft, 20)
+    GPIO.add_event_detect(5, GPIO.FALLING, setTopRight, 20)
+    GPIO.add_event_detect(7, GPIO.FALLING, setBotLeft, 20)
+    GPIO.add_event_detect(8, GPIO.FALLING, setBotRight, 20)
     #GPIO.setup(8,GPIO.OUT, initial=GPIO.LOW)
     GPIO.setup(10, GPIO.IN, GPIO.PUD_DOWN)
 
-def setTopLeft():
+def setTopLeft(channel):
     print("Player moved to space 1!")
 
-def setTopRight():
+def setTopRight(channel):
     print("Player moved to space 2!")
 
-def setBotLeft():
+def setBotLeft(channel):
     print("Player moved to space 3")
 
-def setBotRight():
+def setBotRight(channel):
     print("Player moved to space 4!")
 
 
