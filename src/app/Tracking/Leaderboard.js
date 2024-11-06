2e77d4f3d20acc3f5a13cd94e47f8fe2b9f4866a
Thu78 <thu.nguyen8@utexas.edu>
2024-11-05 18:15:09 -0600

    Score-Tracking feature and Leaderboard

--- src/app/Tracking/Leaderboard.js
+++ src/app/Tracking/Leaderboard.js
@@ -1,27 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './Leaderboard.css';
 
-function Leaderboard (props) {
+function Leaderboard () {
+  const [leaderboard, setLeaderboard] = useState([]);
+
+  useEffect(() => {
+    // Fetch player scores from the backend
+    const fetchScores = async () => {
+      try {
+        const response = await axios.get("http://localhost:5001/get_leaderboard");
+        setLeaderboard(response.data);
+        console.log("Fetch sorted_leaderboard data:", response.data);
+      } catch (error) {
+        console.error("Error fetching leaderboard:", error);
+      }
+    };
+
+    fetchScores();
+  }, []);
+
   return (
-    <div className = "board-container">
-        <h1>
-          Leaderboard
-        </h1>
-        <div className = "stats">
-          {props.playerNames.map((player, index) => {
-            return (
-              <div className="row" key={index}>
-                <span className="column">{index + 1}.</span>
-                <span className="column player-name">{player}</span>
-                <span className="column player-score">0</span>
-            </div>
-            );
-          })}
-        </div>
-        
+    <div className="board-container">
+      <h1>Leaderboard</h1>
+      <div className="stats">
+        {leaderboard.map(([player, score], index) => (
+          <div className="row" key={index}>
+            <span className="column player-name">{player}</span>
+            <span className="column player-score">{score}</span>
+          </div>
+        ))}
+      </div>
     </div>
-    
   );
-}  
+}
+//   return (
+//     <div className = "board-container">
+//         <h1>
+//           Leaderboard
+//         </h1>
+//         <div className = "stats">
+//           {/* {props.playerNames.map((player, index) => { */}
+
+//             return (
+//               <div className="row" key={index}>
+//                 {/* <span className="column">{index + 1}.</span> */}
+//                 <span className="column player-name">{player}</span>
+//                 <span className="column player-score">0</span>
+//             </div>
+//             );
+//           })}
+//         </div>
+        
+//     </div>
+    
+//   );
+// }  
 
 export default Leaderboard;
