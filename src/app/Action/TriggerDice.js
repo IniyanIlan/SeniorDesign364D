630d9dc919a6d900cfdf5d4d3d21b41afafd63ce
RPI5 <tidesoftreachery2025@gmail.com>
2024-11-11 19:47:38 +0000

    fix: fixing demo bugs

--- src/app/Action/TriggerDice.js
+++ src/app/Action/TriggerDice.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useState } from 'react';
+import { useNavigate, useLocation } from "react-router";
 import axios from 'axios';
 
 
 function Trigger(){
 
-
+    const { state } = useLocation();
+    const playerNames = state.playerNames || [];
+    const currentPlayerIndex = state.currentPlayerIndex;
     const [numPips, setNumPips] = useState(0)
 
     const handleTrigger = async () => {
@@ -24,14 +27,33 @@ function Trigger(){
             }
             setNumPips(0);  // Set default or handle accordingly
         }
+        handleValidateMovement()
     }
-
+    const handleValidateMovement = async () => {
+        console.log("Validating Move")
+        try{
+            const res = await axios.get(`http://localhost:5001/validate-move/${currentPlayerIndex}`)
+            const valid = res.data.valid
+            if(valid){
+                console.log("Player has made a valid move to ")
+            }
+        }
+        catch(error){
+            if (error.code === 'ECONNABORTED' || error.response?.status === 408) {
+                console.error("Timeout error: The request took too long to complete.");
+            } 
+            else{
+                console.error("Error: unable to validate move");
+            }
+            setNumPips(0);  // Set default or handle accordingly
+        }
+    }
     return (
         <div>
             <button className='button' onClick={handleTrigger}>
                 Move
             </button>
-            {numPips !== null && <h2>You rolled a {numPips}</h2>}
+            {numPips !== 0 && <h2>You rolled a {numPips}</h2>}
         </div>
     );
 }
