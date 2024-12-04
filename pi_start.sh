01dfbac3450ce6f620ab427e1eb60c44b0ef0837
RPI5 <tidesoftreachery2025@gmail.com>
2024-12-04 06:58:22 +0000

    jk ily <3

--- pi_start.sh
+++ pi_start.sh
@@ -17,8 +17,8 @@ source ./venv/bin/activate
 python3 app.py &
 FLASK_PID=$!
 
-#python3 DiceReading_CurrentLightFaces.py &
-#DICE_PID=$!
+python3 DiceReading_CurrentLightFaces.py &
+DICE_PID=$!
 
 python3 GameBoardMatrix.py &
 BOARD_PID=$!
@@ -32,7 +32,7 @@ cleanup() {
   echo "Stopping all processes..."
   kill $REACT_PID
   kill $FLASK_PID
-  #kill $DICE_PID
+  kill $DICE_PID
   kill $BOARD_PID
   kill $NEO_PID
   echo "All processes stopped."
